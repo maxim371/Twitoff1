a063f17b8346f975285c0e42edf70e073fbc8dde
maxim371 <kwaku.dr@gmail.com>
2020-02-16 00:45:05 -0500

    updated models and added twitter files

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,6 @@
 ''' main application and routing doc for twitoff.'''
 
-#from decouple import config
+from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 
@@ -11,7 +11,10 @@ def create_app():
     app = Flask(__name__)
 
     #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+
+    #stop tracking modifications on sqlalchemy config
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
 
     #have the database know the app
     DB.init_app(app)
@@ -19,5 +22,12 @@ def create_app():
     @app.route('/')
     def root():
         users = User.query.all()
-        return render_template('base.html', title='Home', users=users)
+        return render_template('base.html', title = 'Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+
     return app
